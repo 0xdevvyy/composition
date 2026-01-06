8d39e709d35a7d9db7efd6b9ae5e7972d54a2397
0xdevvyy <pullhead45@gmail.com>
2026-01-06 14:55:13 +0800

    feat: learning about emits and props in composition api

--- composition/src/views/HomeView.vue
+++ composition/src/views/HomeView.vue
@@ -1,6 +1,11 @@
 <script setup>
+  import { ref } from 'vue';
  import { useFlash } from '@/composables/useFlash';
+ import TabableTextArea from '@/components/TabableTextArea.vue';
   let {flash} = useFlash()
+  let comment = ref('test value');
+
+  
 </script>
 
 <template>
@@ -8,5 +13,6 @@
    <p>
     <button @click="flash('it works')">click me</button>
    </p>
+   <TabableTextArea  style="width: 100%; height: 300px;" v-model="comment"/>
   </main>
 </template>
